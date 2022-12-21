d9ad0b8e07956f2bc4de077569f03287fba2c3c1
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2022-12-21 10:53:31 -0800

    added kotlin and java version

---
+++ Kotlin_Computer_Vision/app/src/main/java/com/computervision/kotlin_computer_vision/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.computervision.kotlin_computer_vision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
